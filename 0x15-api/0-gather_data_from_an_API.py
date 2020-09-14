09c4fe401fa379e4d3c984849db3edefc85c0620
Katorea132 <katorea132@gmail.com>
2020-09-14 15:49:45 -0500

    Replacing for get methods

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,16 +21,16 @@ if __name__ == '__main__':
     if nanisore.status_code == 404:
         print("Dare sore omae")
         exit(1)
-    yourName = nanisore.json()['name']
+    yourName = nanisore.json().get('name')
     Nandemonaiya = requests.get(kimiNoNaWa + 'todos',
                                 'userId=' + Makoto).json()
     Sparkle = []
     tot, Zenzenzense = 0, 0
     for yappari in Nandemonaiya:
         tot += 1
-        if yappari['completed']:
+        if yappari.get('completed'):
             Zenzenzense += 1
-            Sparkle.append(yappari['title'])
+            Sparkle.append(yappari.get('title'))
     Nandemonaiya = tot  # Needed to keep the references :3
     Sparkle = '\n'.join(['\t ' + Mitsuha for Mitsuha in Sparkle])
     print(radwimps.format(yourName, Zenzenzense, Nandemonaiya))
